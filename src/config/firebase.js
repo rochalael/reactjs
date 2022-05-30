0c6a24ce740ec283fbbba0838a036c9817973fd5
RochaLael <laelrocha@hotmail.com>
2022-05-29 23:30:18 -0300

    29052022_up

---
+++ src/config/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseApp = initializeApp({
+
+  apiKey: "AIzaSyAwU1NbC-5UNC482uCDz6-zzPpz58qVdDM",
+
+  authDomain: "mobile-nc.firebaseapp.com",
+
+  projectId: "mobile-nc",
+
+  storageBucket: "mobile-nc.appspot.com",
+
+  messagingSenderId: "774739276650",
+
+  appId: "1:774739276650:web:492820b3a4083f4adfeb13"
+
+});
+
+export default firebaseApp;
--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
+import firebase from '../../config/firebase';
+import 'firebase/auth';
 
 function Login(){
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function logar(){
+
+        firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
+            alert('usuário logado!');
+        }).catch(erro => {
+            alert(erro);
+        })
+    }
+
     return(
             <div className="login-content d-flex align-items-center">
+
                 <form className="form-signin mx-auto">
-                <div className="text-center mb-4">
-                <h1 className="h3 mb-3 font-weight-normal">Login</h1>
+                    <div className="text-center mb-4">
+                    <h1 className="h3 mb-3 font-weight-normal font-weight-bold">Login</h1> 
                 </div>
 
-                <input type="email" id="inputEmail" class="form-control my-2" placeholder="Email address" />
-                <input type="password" id="inputPassword" class="form-control my-2" placeholder="Password" />
+                <input onChange={(e) => setEmail(e.target.value) } type="email" id="inputEmail" class="form-control my-2" placeholder="Email" />          
+                <input onChange={(e) => setSenha(e.target.value) } type="password" id="inputPassword" class="form-control my-2" placeholder="Senha" />    
             
-                <button class="btn btn-lg btn-primary btn-block btn-login" type="submit">Sign in</button>
+                <button onClick={logar} class="btn btn-lg btn-block text-white btn-login" type="button">Entrar</button>
 
-                <p class="mt-5 mb-3 text-muted text-center">&copy; 2022</p>
+                <div className="opcoes-login mt-3 text-center">
+                        <a href="#" className="mx-2 font-weight-bold">Recuperar Senha</a>
+                        <a href="#" className="mx-2 font-weight-bold">Quero Cadastrar</a>
+                </div>
+
                 </form>
             </div>
     )
